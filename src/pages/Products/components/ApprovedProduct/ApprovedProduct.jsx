d48c7fc609cb7ddee5067bf63844a3da27bf3125
sohan004 <aryansohan02@gmail.com>
2024-05-31 01:41:34 +0600

    sdfdf

--- src/pages/Products/components/ApprovedProduct/ApprovedProduct.jsx
+++ src/pages/Products/components/ApprovedProduct/ApprovedProduct.jsx
@@ -138,7 +138,7 @@ const ApprovedProduct = () => {
                                         onChange={() => product?.status === 'active' ? inActive(product?._id) : active(product?._id)}
                                         checked={product?.status === 'active' ? true : false}
                                         className="h-0 w-0 overflow-hidden peer" />
-                                    <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+                                    <div className="relative w-11 h-6 bg-gray-200  peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
                                 </label>
                             </td>
                             <td>
